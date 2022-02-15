ea1871085a37a519699cf9006145817649428327
Anna <atsigip@gmail.com>
2022-02-15 22:55:52 +0300

    Exp014 sum of digits in a real number

---
+++ Exp014.py
@@ -0,0 +1,9 @@
+# 14.Подсчитать сумму цифр в вещественном числе
+
+num = input("Введите число: ")
+sum = 0
+for i in range(0, len(num)):
+    if num[i] != ".":
+        sum += int(num[i])
+print(sum)
+
