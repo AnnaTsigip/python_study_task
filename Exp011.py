7b2fc9120573d1caf5e240fa7b81c5dbe5ed1816
Anna <atsigip@gmail.com>
2022-02-15 22:09:54 +0300

    Exp011 list of N members of the sequence

---
+++ Exp011.py
@@ -0,0 +1,11 @@
+# Сформировать список из  N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
+
+list = []
+n = 5
+for i in range(n):
+    if i % 2 == 0:
+        list.append(3**i)
+    else:
+        list.append(3**i * -1)
+print(list)
