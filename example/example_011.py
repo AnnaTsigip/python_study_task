b462a83e2e2d33ea5275964fe6ed01e05cd5da3f
Anna <atsigip@gmail.com>
2022-03-29 22:02:32 +0300

    list of N members

--- example/example_011.py
+++ example/example_011.py
@@ -1,15 +1,15 @@
 # Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-list = []
-n = 5
-for i in range(n):
-    if i % 2 == 0:
-        list.append(3**i)
-    else:
-        list.append(3**i * -1)
-print(list)
 
+subsequence = []
+count = int(input('Введите количество членов последовательности: '))
+for i in range(count):
+    if i % 2 == 0:
+        subsequence.append(3**i)
+    else:
+        subsequence.append(3**i * -1)
+print(subsequence)
 
 
 # def create_list(n):
