e7aa671ec25c06f2f1d54b36fadcccae136b593c
Anna <atsigip@gmail.com>
2022-03-30 21:50:47 +0300

    sum of digits in included number

--- example/example_014.py
+++ example/example_014.py
@@ -1,9 +1,24 @@
 # 14.Подсчитать сумму цифр в вещественном числе
 
-num = input("Введите число: ")
-sum = 0
-for i in range(0, len(num)):
-    if num[i] != ".":
-        sum += int(num[i])
-print(sum)
+
+num = float(input('numner = ')) # вводим число
+num_str = str(float(num)) # переводим в строку
+#print(type(num_str)) 
+num_str1 = num_str.split('.') # разбиваем по разделителю
+# print(num_str1)
+num1 = int(num_str1[0]) # записываем до запятой
+num2 = int(num_str1[-1]) # после запятой
+#print(num1)
+#print(num2)
+
+def sum_num(num):
+    sum = 0
+    while num > 0:
+        sum += num % 10
+        num //= 10
+    return(sum)
+
+print(sum_num(num1) + sum_num(num2))
+#print(sum_num(num2))
+
 
