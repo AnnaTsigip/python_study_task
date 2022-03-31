893d126847c7a8bf024495096622f67aaf48eb01
Anna <atsigip@gmail.com>
2022-03-31 20:19:17 +0300

    set of products of numbers from 1 to N

--- example/example_015.py
+++ example/example_015.py
@@ -1,13 +1,18 @@
-# Написать программу получающую набор 
+# Написать программу получающую набор произведений чисел от 1 до N.
 # Пример: пусть N = 4, тогда
 # [ 1, 2, 6, 24 ]
 
+
+# вводим число
+# от 1 до n=count*i
+
+
 list = []
-N = int(input('Введите N: '))
-num = 1
-product = 1
+num=1
+product =1
+N = int(input('N = '))
 for i in range(1, N+1):
-    num = num * i
+    num = num *i
     product = num
     list.append(product)
 print(list)
