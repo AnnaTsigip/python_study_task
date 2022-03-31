6188d3b09d575acb336264e6f6c68a7b339201fe
Anna <atsigip@gmail.com>
2022-03-31 20:43:20 +0300

    the sum of the terms of the sequence (1+1n)**n

--- example/example_016.py
+++ example/example_016.py
@@ -1,16 +1,30 @@
-# Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму
+# # Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму
 
 
 list = []
-num = 1
-sum = 0
-N = int(input('Введите N: '))
+N = int(input('N = '))
 for i in range(1, N+1):
-    num = ((1 + (1/i))**i)
-    sum += num
-    list.append(num)
+    num = (1+(1/i))**i
+    num_list = num
+    list.append(num_list)
 
 print(list)
-print(sum)
+
+def list_sum(listsum):
+    sum =0
+    for i in list:
+        sum = sum + i
+    return sum
+
+print(list_sum(list))
 
 
+
+
+# def listsum(numList):
+#     theSum = 0
+#     for i in numList:
+#         theSum = theSum + i
+#     return theSum
+
+# print(listsum([2.0, 2.25, 2.37037037037037, 2.44140625]))
